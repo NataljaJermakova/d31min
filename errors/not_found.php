88f5954693710d5dbcefb3b16aa946be9e248c44
NataljaJermakova <natalja_jermakova@inbox.lv>
2020-11-25 12:40:38 +0200

    styled the "not-found" page

--- errors/not_found.php
+++ errors/not_found.php
@@ -2,10 +2,57 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport"
+    content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="libraries/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="libraries/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>Error</title>
+    <link rel="icon" href="image/logo.png">
 </head>
 <body>
-    Lapa nav atrasta!
+<div class="container-fluid" style="padding-left: 0; padding-right: 0;">
+        <header>It's MY page</header>
+
+        <nav class="navbar navbar-light navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav" style="width: 540px;">
+                        <li><a href="index#home">Home</a></li>
+                        <li><a href="index#our_team">Our team</a></li>
+                        <li><a href="index#project">Projects</a></li>
+                        <li><a href="index#map">Map</a></li>
+                        <li><a href="contact">Contact</a></li>
+                    </ul>
+                    <form class="form-inline">
+                        <a href="register">Sign Up</a>
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div class="container" style="text-align:center;">
+        <h3>Lapa nav atrasta!</h3>
+        <img src="image\LutraSad.png" alt="Even picture is not found!" width="100%" style="margin-top:-50px;max-width:600px;">
+    </div>
+            
+    <div class="sidebar sticky" style="padding-left:20px;">
+        <ul>
+            <h3><a href="index">Return</a></h3>
+        </ul>
+    </div>
 </body>
 </html>
